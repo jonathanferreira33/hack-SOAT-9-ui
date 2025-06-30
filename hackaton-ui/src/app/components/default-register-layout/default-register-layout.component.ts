04b7b6cbc0790986723be1666703c3949d050383
Jonathan Ferreira <jhouus33@gmail.com>
2025-06-30 13:04:51 -0300

    feat: add arquivos iniciais e estrutura projeto

---
+++ hackaton-ui/src/app/components/default-register-layout/default-register-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-register-layout',
+  imports: [],
+  templateUrl: './default-register-layout.component.html',
+  styleUrl: './default-register-layout.component.scss'
+})
+export class DefaultRegisterLayoutComponent {
+
+}
